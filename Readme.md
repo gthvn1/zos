faa90c60555a0ae936da515aea4206911763fecf
Guillaume <gthvn1@gmail.com>
2025-01-25 14:50:58 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Zig Operating System
+
+## Links
+
+- [Operating System in 1,000 Lines](https://operating-system-in-1000-lines.vercel.app/en/)
---
+++ shell.nix
@@ -0,0 +1,15 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.11";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    zig
+    zls
+    qemu
+    helix
+  ];
+
+COLORTERM = "truecolor";
+}
